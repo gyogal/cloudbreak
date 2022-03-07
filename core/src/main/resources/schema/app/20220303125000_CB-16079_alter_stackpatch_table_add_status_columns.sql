61f19d0981a60032e51a84648192e0f3a13844d4
David Bajzath <bajzathd@gmail.com>
2022-03-07 17:59:41 +0100

    CB-16079 Do not schedule finalized stack patches
    Stack patcher should not schedule jobs for stacks that are already finalized: either unsheduled, not affected or already fixed.

---
+++ core/src/main/resources/schema/app/20220303125000_CB-16079_alter_stackpatch_table_add_status_columns.sql
@@ -0,0 +1,13 @@
+-- // CB-16079
+-- Migration SQL that makes the change goes here.
+
+ALTER TABLE stackpatch ADD COLUMN IF NOT EXISTS status CHARACTER VARYING(255);
+UPDATE stackpatch SET status = 'FIXED' WHERE status IS NULL;
+ALTER TABLE stackpatch ALTER COLUMN status SET NOT NULL;
+
+ALTER TABLE stackpatch ADD COLUMN IF NOT EXISTS status_reason TEXT;
+
+-- //@UNDO
+-- SQL to undo the change goes here.
+
+ALTER TABLE stackpatch ALTER COLUMN status DROP NOT NULL;
