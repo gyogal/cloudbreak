b14a104c186f3a7414770b84df0de66822455fcb
Annamaria Szegedi <aszegedi@cloudera.com>
2022-03-21 13:40:43 +0100

    CB-16458 E2E tests for Resilient Scaling with percentage option

--- integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/distrox/DistroXScaleTestParameters.java
+++ integration-test/src/main/java/com/sequenceiq/it/cloudbreak/testcase/e2e/distrox/DistroXScaleTestParameters.java
@@ -1,6 +1,10 @@
 package com.sequenceiq.it.cloudbreak.testcase.e2e.distrox;
 
+import java.util.Arrays;
 import java.util.Map;
+
+import com.sequenceiq.common.api.type.AdjustmentType;
+import com.sequenceiq.it.cloudbreak.exception.TestFailException;
 
 public class DistroXScaleTestParameters {
 
@@ -14,6 +18,10 @@ public class DistroXScaleTestParameters {
 
     private static final String IRRELEVANT_HOST_GROUP = "irrelevant_host_group";
 
+    private static final String ADJUSTMENT_TYPE = "adjustment_type";
+
+    private static final String THRESHOLD = "threshold";
+
     private static final String DEFAULT_TIMES = "2";
 
     private static final String DEFAULT_SCALE_UP_TARGET = "6";
@@ -23,6 +31,10 @@ public class DistroXScaleTestParameters {
     private static final String DEFAULT_HOST_GROUP = "worker";
 
     private static final String DEFAULT_IRRELEVANT_HOST_GROUP = "compute";
+
+    private static final String DEFAULT_ADJUSTMENT_TYPE = "Exact";
+
+    private static final String DEFAULT_THRESHOLD = "0";
 
     private int times;
 
@@ -34,12 +46,18 @@ public class DistroXScaleTestParameters {
 
     private String irrelevantHostGroup;
 
+    private AdjustmentType adjustmentType;
+
+    private long threshold;
+
     DistroXScaleTestParameters(Map<String, String> allParameters) {
         setHostGroup(allParameters.getOrDefault(HOST_GROUP, DEFAULT_HOST_GROUP));
         setScaleUpTarget(Integer.parseInt(allParameters.getOrDefault(SCALE_UP_TARGET, DEFAULT_SCALE_UP_TARGET)));
         setScaleDownTarget(Integer.parseInt(allParameters.getOrDefault(SCALE_DOWN_TARGET, DEFAULT_SCALE_DOWN_TARGET)));
         setTimes(Integer.parseInt(allParameters.getOrDefault(TIMES, DEFAULT_TIMES)));
         setIrrelevantHostGroup(allParameters.getOrDefault(IRRELEVANT_HOST_GROUP, DEFAULT_IRRELEVANT_HOST_GROUP));
+        setAdjustmentType(allParameters.getOrDefault(ADJUSTMENT_TYPE, DEFAULT_ADJUSTMENT_TYPE));
+        setThreshold(Long.parseLong(allParameters.getOrDefault(THRESHOLD, DEFAULT_THRESHOLD)));
     }
 
     public int getScaleUpTarget() {
@@ -80,5 +98,25 @@ public class DistroXScaleTestParameters {
 
     public void setIrrelevantHostGroup(String irrelevantHostGroup) {
         this.irrelevantHostGroup = irrelevantHostGroup;
+    }
+
+    public AdjustmentType getAdjustmentType() {
+        return adjustmentType;
+    }
+
+    public void setAdjustmentType(String adjustmentTypeName) {
+        adjustmentType = Arrays.stream(AdjustmentType.values())
+                .filter(adjustmentType -> adjustmentType.name().equalsIgnoreCase(adjustmentTypeName))
+                .findFirst()
+                .orElseThrow(() -> new TestFailException(String.format("There is no scaling AdjustmentType for '%s'",
+                        adjustmentTypeName)));
+    }
+
+    public long getThreshold() {
+        return threshold;
+    }
+
+    public void setThreshold(long threshold) {
+        this.threshold = threshold;
     }
 }
